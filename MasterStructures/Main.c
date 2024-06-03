e2fcf4aab16378f7108c6749a93d22ed49595696
Ispas Anisia <ispasanisia22@stud.ase.ro>
2024-06-03 12:44:15 +0300

    Linked Project. Already had a sliiiiight bit done such as an informational struct I will use with the rest of the structures

---
+++ MasterStructures/Main.c
@@ -0,0 +1,23 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <malloc.h>
+#include <stdlib.h>
+#include <string.h>
+#include "Basic.h"
+
+
+
+int main() 
+{
+	//quickly testing if printStruct works
+	/*InformationStruct info;
+	info.stringslol = (char**)malloc(sizeof(char*) * 3);
+	for (int i = 0; i < 3; i++) info.stringslol[i] = (char*)malloc(sizeof(char) * 6);
+	info.nr = 99, info.funnydub = 0.001, info.stringslol[0] = "ajsdhs", info.stringslol[1] = "string", info.stringslol[2] = NULL;
+	printStruct(info);*/
+
+	
+
+
+}
+
